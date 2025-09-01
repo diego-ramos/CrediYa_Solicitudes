031bd97fa0cab6d32154e4a199b838390bec7362
El_Coloso <diegoramosp@gmail.com>
2025-08-31 22:14:09 -0500

    Update swagger

--- infrastructure/entry-points/reactive-web/src/main/java/com/crediya/api/config/SecurityConfig.java
+++ infrastructure/entry-points/reactive-web/src/main/java/com/crediya/api/config/SecurityConfig.java
@@ -30,6 +30,8 @@ public class SecurityConfig {
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchanges -> exchanges
                         .pathMatchers("/api/v*/solicitud/new").hasAnyRole(CUSTOMER_ROLE)
+                        .pathMatchers("/webjars/swagger-ui/*").permitAll()
+                        .pathMatchers("/v3/api-docs/*").permitAll()
                         .anyExchange().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
